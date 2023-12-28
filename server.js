c3ad0e03d777cc424115a63cc7e56d86d20769a4
Reni <123961028+SpaceBlobs@users.noreply.github.com>
2023-12-28 10:08:55 +0000

    /

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// Interceptor
+const WebSocket = require('ws');
+const url = require('url');
+
+const wsServer = new WebSocket.Server({ port: 8080 });
+
+wsServer.on('connection', (client, req) => {
+  //const urlParams = url.parse(req.url, true).query;
+  console.log('Client connected');
+  
+  //if(urlParams.targetUrl) {
+    //console.log(urlParams.targetUrl)
+  //}
+
+  client.on('message', (message) => {
+    const uint8ArrayData = new Uint8Array(Buffer.from(message));
+    console.log(uint8ArrayData);
+  });
+
+  client.on('close', () => {
+    console.log('Client disconnected');
+  });
+
+});
+
+console.log('WebSocket server is running on port 8080');
